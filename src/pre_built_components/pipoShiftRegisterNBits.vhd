a246a7dfa2e1a37940c70b88761869c06c6b16e5
SplitWolf <mat.jake1360@gmail.com>
2024-12-23 13:59:30 -0500

    refactor: Combine left/right shift signal, add clock enable

--- src/pre_built_components/pipoShiftRegisterNBits.vhd
+++ src/pre_built_components/pipoShiftRegisterNBits.vhd
@@ -7,16 +7,16 @@ use basic_rtl.all;
 
 entity pipoShiftRegisterNBits is
     generic(
-        DataWidth: integer := 1
+        DataWidth: natural := 1
     );
     port (
         i_in: in std_logic_vector ((DataWidth-1) downto 0); 
         i_serialIn: in std_logic;
-        i_shiftLeft: in std_logic; 
-        i_shiftRight: in std_logic; 
-        i_clear: in std_logic;
+        i_shitLeftn_shiftRight: in std_logic;
         i_load: in std_logic;
-        i_clock: in std_logic;
+        i_clk: in std_logic;
+        i_cen: in std_logic;
+        i_resetn: in std_logic;
         o_serialOut: out std_logic;
         o_out: out std_logic_vector ((DataWidth-1) downto 0)
     );
@@ -51,12 +51,10 @@ end component;
 
 signal int_mux_sel: std_logic_vector(1 downto 0);
 signal int_shift_left, int_shift_right, int_out, int_mux_out: std_logic_vector(DataWidth - 1 downto 0);
-signal int_load: std_logic;
+
 begin
 
-int_mux_sel <= i_shiftRight & i_shiftLeft;
-int_load <= i_shiftRight or i_shiftLeft or i_load;
-assert ((int_mux_sel /= "11")) report "Trying to shift both directions at the same time" severity ERROR;
+int_mux_sel <= i_load & i_shitLeftn_shiftRight;
 
 int_shift_left <= int_out(DataWidth - 2 downto 0) & i_serialIn;
 int_shift_right <= i_serialIn & int_out(DataWidth - 1 downto 1);
@@ -66,10 +64,10 @@ mode_sel: entity basic_rtl.busMux4x1
     DataWidth => DataWidth
 )
  port map(
-    i_in0 => i_in,
-    i_in1 => int_shift_left,
-    i_in2 => int_shift_right,
-    i_in3 => int_out,
+    i_in0 => int_shift_left,
+    i_in1 => int_shift_right,
+    i_in2 => i_in,
+    i_in3 => i_in,
     i_sel => int_mux_sel,
     o_out => int_mux_out
 );
@@ -79,14 +77,15 @@ registerNbits_inst: entity basic_rtl.registerNbits
 )
  port map(
     i_in => int_mux_out,
-    i_clear => i_clear,
-    i_load => int_load,
-    i_clock => i_clock,
+    i_load => '1',
+    i_clk => i_clk,
+    i_cen => i_cen,
+    i_resetn => i_resetn,
     o_out => int_out
 );
 
 -- Ouput Drivers
 o_out <= int_out;
-o_serialOut <= int_out(DataWidth-1);
+o_serialOut <= (int_out(DataWidth-1) and not i_shitLeftn_shiftRight) or (int_out(0) and i_shitLeftn_shiftRight);
 
-end architecture;
+end architecture; -- rtl
